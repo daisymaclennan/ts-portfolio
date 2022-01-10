cf4069eb10fc02e0569ee620120536b78c2c7222
Daisy ;) <daisylouisemaclennan@gmail.com>
2022-01-10 11:01:10 +0000

    chnage around to over

--- data/index.tsx
+++ data/index.tsx
@@ -1,6 +1,6 @@
 const data = {
   introduction:
-    "I'm a Front End Developer based in Derby, UK. I have around 3 years experience working with world class agencies across the UK.",
+    "I'm a Front End Developer based in Derby, UK. I have over 3 years experience working with world class agencies across the UK.",
   skills: {
     sm: ["styledcomps.png", "vue.png", "wordpress.png"],
     md: ["gatsby.png", "js.jpeg"],
