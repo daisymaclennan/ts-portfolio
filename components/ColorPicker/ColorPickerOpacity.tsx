86cf9f400e5527f462c17be4ca104446c78b14ec
daisy :) <daisylouisemaclennan@gmail.com>
2022-03-15 20:37:06 +0000

    feature: add basic opacity slider

--- components/ColorPicker/ColorPickerOpacity.tsx
+++ components/ColorPicker/ColorPickerOpacity.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { useRef } from "react";
 import styled from "styled-components";
 
 interface ColorPickerOpacityProps {
@@ -12,10 +12,28 @@ const ColorPickerOpacity = ({
   color,
   setColor,
 }: ColorPickerOpacityProps) => {
+  const constraints = useRef<HTMLDivElement>(null);
   return (
-    <div className={className}>
-      <motion.button type="button" />
-      <div />
+    <div className={className} ref={constraints}>
+      <input
+        type="range"
+        min={0}
+        max={1}
+        step={0.05}
+        value={color.array[3]}
+        onChange={(e) =>
+          setColor({
+            actual: `rgba(${color.array[0]}, ${color.array[1]}, ${color.array[2]}, ${e.target.value})`,
+            array: [
+              color.array[0],
+              color.array[1],
+              color.array[2],
+              e.target.value,
+            ],
+          })
+        }
+        aria-label="Max users needed"
+      />
     </div>
   );
 };
@@ -26,7 +44,7 @@ const StyledColorPickerOpacity = styled(ColorPickerOpacity)`
   background: white;
   border-radius: 20px;
   position: relative;
-  background-image: url('https://static.vecteezy.com/system/resources/previews/003/793/482/large_2x/transparent-grid-pattern-for-background-vector.jpg');
+  background-image: url("https://static.vecteezy.com/system/resources/previews/003/793/482/large_2x/transparent-grid-pattern-for-background-vector.jpg");
   background-size: cover;
 
   button {
@@ -37,21 +55,57 @@ const StyledColorPickerOpacity = styled(ColorPickerOpacity)`
     border: none;
     background: white;
     box-shadow: 0 0 1rem 0.4rem rgba(0, 0, 0, 0.15);
+    cursor: pointer;
+    position: relative;
+    z-index: 100;
   }
 
-  div {
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
+  input[type="range"] {
+    -webkit-appearance: none;
+    background: transparent;
     width: 100%;
-    border-radius: 20px;
-    background: linear-gradient(
-      to right,
-      ${(props) =>
-        props.color &&
-        `rgba(${props.color.array[0]}, ${props.color.array[1]}, ${props.color.array[2]}, 0), rgba(${props.color.array[0]}, ${props.color.array[1]}, ${props.color.array[2]}, 1)`}
-    );
+
+    &::-webkit-slider-thumb {
+      -webkit-appearance: none;
+      border: none;
+      width: 3.1rem;
+      height: 1.3rem;
+      border-radius: 999px;
+      background-color: pink;
+      cursor: grab;
+      margin-top: -0.4rem;
+      background-image: url("data:image/svg+xml,%0A%3Csvg width='133' height='57' viewBox='0 0 133 57' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M32 23L38 29L44 23L46 25L38 33L30 25L32 23Z' fill='black'/%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M27.999 0H105C120.463 0 132.999 12.536 132.999 28V29C132.999 44.463 120.463 57 105 57H27.999C12.536 57 0 44.463 0 29V28C0 12.536 12.536 0 27.999 0Z' fill='%23FB6664'/%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M44 16.75L32.6 28L44 39.25L40.2 43L25 28L40.2 13L44 16.75Z' fill='white'/%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M89 16.75L100.4 28L89 39.25L92.8 43L108 28L92.8 13L89 16.75Z' fill='white'/%3E%3C/svg%3E%0A");
+      background-size: 100% 100%;
+      background-repeat: no-repeat;
+    }
+
+    &::-moz-range-thumb {
+      -webkit-appearance: none;
+      border: none;
+      width: 3.1rem;
+      height: 1.3rem;
+      border-radius: 999px;
+      background-color: pink;
+      cursor: grab;
+      margin-top: -0.4rem;
+      background-image: url("data:image/svg+xml,%0A%3Csvg width='133' height='57' viewBox='0 0 133 57' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M32 23L38 29L44 23L46 25L38 33L30 25L32 23Z' fill='black'/%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M27.999 0H105C120.463 0 132.999 12.536 132.999 28V29C132.999 44.463 120.463 57 105 57H27.999C12.536 57 0 44.463 0 29V28C0 12.536 12.536 0 27.999 0Z' fill='%23FB6664'/%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M44 16.75L32.6 28L44 39.25L40.2 43L25 28L40.2 13L44 16.75Z' fill='white'/%3E%3Cpath fill-rule='evenodd' clip-rule='evenodd' d='M89 16.75L100.4 28L89 39.25L92.8 43L108 28L92.8 13L89 16.75Z' fill='white'/%3E%3C/svg%3E%0A");
+      background-size: 100% 100%;
+      background-repeat: no-repeat;
+    }
+
+    &::-webkit-slider-runnable-track {
+      width: 100%;
+      height: 0.5rem;
+      background: white;
+      border-radius: 999px;
+    }
+
+    &::-moz-range-track {
+      width: 100%;
+      height: 0.5rem;
+      background: white;
+      border-radius: 999px;
+    }
   }
 `;
 
