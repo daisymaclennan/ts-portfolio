545d4d89fc23f63cc8f9456865ac2ccc3456ec63
daisy :) <daisylouisemaclennan@gmail.com>
2022-03-15 17:45:04 +0000

    feature: add color picker button and bg color

--- components/ColorPicker/ColorPickerBig.tsx
+++ components/ColorPicker/ColorPickerBig.tsx
@@ -2,38 +2,40 @@ import { MouseEvent, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
 const drawColorSpectrum = (canvasContext, canvas) => {
-    let gradient = canvasContext.createLinearGradient(0, 0, canvas.width, 0);
-    gradient.addColorStop(0, "#ff0000");
-    gradient.addColorStop(1 / 6, "#ffff00");
-    gradient.addColorStop((1 / 6) * 2, "#00ff00");
-    gradient.addColorStop((1 / 6) * 3, "#00ffff");
-    gradient.addColorStop((1 / 6) * 4, "#0000ff");
-    gradient.addColorStop((1 / 6) * 5, "#ff00ff");
-    gradient.addColorStop(1, "#ff0000");
-    canvasContext.fillStyle = gradient;
-    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+  let gradient = canvasContext.createLinearGradient(0, 0, canvas.width, 0);
+  gradient.addColorStop(0, "#ff0000");
+  gradient.addColorStop(1 / 6, "#ffff00");
+  gradient.addColorStop((1 / 6) * 2, "#00ff00");
+  gradient.addColorStop((1 / 6) * 3, "#00ffff");
+  gradient.addColorStop((1 / 6) * 4, "#0000ff");
+  gradient.addColorStop((1 / 6) * 5, "#ff00ff");
+  gradient.addColorStop(1, "#ff0000");
+  canvasContext.fillStyle = gradient;
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 
-    gradient = canvasContext.createLinearGradient(0, 0, 0, canvas.height);
-    gradient.addColorStop(0, "rgba(255, 255, 255, 1)");
-    gradient.addColorStop(0.5, "rgba(255, 255, 255, 0)");
-    gradient.addColorStop(1, "rgba(255, 255, 255, 0)");
-    canvasContext.fillStyle = gradient;
-    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+  gradient = canvasContext.createLinearGradient(0, 0, 0, canvas.height);
+  gradient.addColorStop(0, "rgba(255, 255, 255, 1)");
+  gradient.addColorStop(0.5, "rgba(255, 255, 255, 0)");
+  gradient.addColorStop(1, "rgba(255, 255, 255, 0)");
+  canvasContext.fillStyle = gradient;
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
 
-    gradient = canvasContext.createLinearGradient(0, 0, 0, canvas.height);
-    gradient.addColorStop(0, "rgba(0, 0, 0, 0)");
-    gradient.addColorStop(0.5, "rgba(0, 0, 0, 0)");
-    gradient.addColorStop(1, "rgba(0, 0, 0, 1)");
-    canvasContext.fillStyle = gradient;
-    canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-}
+  gradient = canvasContext.createLinearGradient(0, 0, 0, canvas.height);
+  gradient.addColorStop(0, "rgba(0, 0, 0, 0)");
+  gradient.addColorStop(0.5, "rgba(0, 0, 0, 0)");
+  gradient.addColorStop(1, "rgba(0, 0, 0, 1)");
+  canvasContext.fillStyle = gradient;
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+};
 interface ColorPickerProps {
   className?: string;
+  color: string;
   setColor: (color: string) => void;
 }
-const ColorPicker = ({ className, setColor }: ColorPickerProps) => {
+const ColorPicker = ({ className, color, setColor }: ColorPickerProps) => {
   const canvas = useRef<HTMLCanvasElement>(null);
   const [canvasContext, setCanvasContext] = useState<any>(null);
+  const [btnPos, setBtnPos] = useState({ x: 0, y: 0 });
 
   useEffect(() => {
     if (canvas.current) {
@@ -41,6 +43,7 @@ const ColorPicker = ({ className, setColor }: ColorPickerProps) => {
       drawColorSpectrum(canvas.current.getContext("2d"), canvas.current);
     }
   }, [canvasContext]);
+
   const handleColorChange = (e: MouseEvent<HTMLCanvasElement>) => {
     console.log(e);
     if (canvas.current) {
@@ -68,23 +71,38 @@ const ColorPicker = ({ className, setColor }: ColorPickerProps) => {
   };
   return (
     <div className={className}>
-      <canvas ref={canvas} onClick={(e) => handleColorChange(e)} />
+      <button
+        type="button"
+        style={{ backgroundColor: color }}
+      />
+      <div>
+        <canvas ref={canvas} onClick={(e) => handleColorChange(e)} />
+      </div>
     </div>
   );
 };
 const StyledColorPicker = styled(ColorPicker)`
-  height: 0;
-  padding-top: calc(867 / 1282 * 100%);
-  background: white;
-  border-radius: 20px;
-  overflow: hidden;
   position: relative;
-  canvas {
-    height: 100%;
-    width: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
+  button {
+    height: 22px;
+    width: 22px;
+    border-radius: 11px;
+    border: 2px solid white;
+  }
+  div {
+    height: 0;
+    padding-top: calc(867 / 1282 * 100%);
+    background: white;
+    border-radius: 20px;
+    overflow: hidden;
+    position: relative;
+    canvas {
+      height: 100%;
+      width: 100%;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
   }
 `;
 export default StyledColorPicker;
