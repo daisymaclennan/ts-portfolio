8e270e43b6b9c1ca303f29cf473e1e423a38d262
Daisy <daisylouisemaclennan@gmail.com>
2023-10-26 19:08:02 +0100

    fix: svg icon

--- components/Icon/Arrow.tsx
+++ components/Icon/Arrow.tsx
@@ -4,22 +4,10 @@ function Icon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
+      fillRule="evenodd"
+      clipRule="evenodd"
     >
-      <path
-        stroke="#FFFFFF"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        strokeWidth="2"
-        d="M14.283 5L21 12l-6.717 7"
-      ></path>
-      <path
-        stroke="#FFFFFF"
-        strokeLinecap="round"
-        strokeWidth="2"
-        d="M19.732 12.032L3.999 12.032"
-      ></path>
+      <path d="M21.883 12l-7.527 6.235L15 19l9-7.521L15 4l-.645.764L21.884 11H0v1h21.883z"></path>
     </svg>
   );
 }
